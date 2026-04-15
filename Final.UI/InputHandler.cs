b68e4acf21b59170bea4438d76937fe3ed0ff337
Paola Mora <paola.moram2005@hotmail.com>
2026-04-14 22:34:16 -0600

    feat: added InputHandler

--- Final.UI/InputHandler.cs
+++ Final.UI/InputHandler.cs
@@ -2,5 +2,35 @@ namespace Final.UI;
 
 public class InputHandler
 {
-    
+    private string _input = string.Empty;
+    private bool _isValid;
+
+    public bool ValidateInput(string input)
+    {
+        _input = input;
+        _isValid = !string.IsNullOrWhiteSpace(_input);
+        return _isValid;
+    }
+
+    public string ReadInput()
+    {
+        while (true)
+        {
+            _input = Console.ReadLine() ?? string.Empty;
+            _isValid = ValidateInput(_input);
+
+            if (!_isValid)
+            {
+                break;
+            }
+            
+            DisplayError();
+        }
+        return _input;
+    }
+
+    public void DisplayError()
+    {
+        System.Console.WriteLine("Invalid input. Try again.");
+    }
 }
