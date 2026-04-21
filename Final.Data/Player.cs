94a360f7a83aea83cd4bfc8c020e6d4f3253f60f
Paola Mora <paola.moram2005@hotmail.com>
2026-04-20 21:46:46 -0600

    feat: added player class

--- Final.Data/Player.cs
+++ Final.Data/Player.cs
@@ -1,6 +1,16 @@
-﻿namespace Final.Data;
+﻿using Final.Logic;
+
+namespace Final.Data;
 
 public class Player
 {
-
+    public string Username { get; private set;}
+    public int CurrentLevel {get; set;}
+    public InventoryManager Inventory {get; private set;}
+    public Player(string username)
+    {
+        Username = username;
+        CurrentLevel = 0;
+        Inventory = new InventoryManager();
+    }
 }
