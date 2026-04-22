a1a91b69049fea3a69284f6bb60db0b7a49f4a9c
Paola Mora <paola.moram2005@hotmail.com>
2026-04-22 17:17:38 -0600

    fix: change structure to be according to the documentation

--- Final.Logic/GameManager.cs
+++ Final.Logic/GameManager.cs
@@ -2,5 +2,44 @@
 
 public class GameManager
 {
+    public void StartGame()
+    {
+        var map = new MapManager(20, 20, 1, 1);
 
+        bool playing = true;
+
+        while (playing)
+        {
+            Console.Clear();
+            map.DisplayMap();
+            System.Console.WriteLine();
+            System.Console.WriteLine("Use W/A/S/D to move. Press Q to return to the menu.");
+
+            var keyInfo = Console.ReadKey(true);
+            string key = keyInfo.Key.ToString().ToUpper();
+
+            if (key == "Q")
+            {
+                playing = false;
+                continue;
+            }
+
+            if (key is "W" or "A" or "S" or "D")
+            {
+                bool moved = map.MovePlayer(key);
+                if (!moved)
+                {
+                    System.Console.WriteLine("You can't move there!");
+                    System.Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey(true);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid key. Use W/A/S/D or Q.");
+                System.Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+        }
+    }
 }
--- Final.UI/Program.cs
+++ Final.UI/Program.cs
@@ -1,4 +1,41 @@
+using Final.Data;
+using Final.Logic;
 using Final.UI;
 
 var menu = new MenuDisplay();
-menu.MainMenu();
+var saveSystem = new SaveSystem();
+var gameManager = new GameManager();
+
+string username = menu.LoginScreen();
+Player currentPlayer = saveSystem.LoadGame(username) ?? new Player(username);
+
+if (!saveSystem.SaveExists(username))
+{
+    saveSystem.SaveGame(currentPlayer);
+}
+
+bool inMenu = true;
+
+while (inMenu)
+{
+    string option = menu.ShowMainMenu(currentPlayer);
+
+    switch (option)
+    {
+        case "1":
+            gameManager.StartGame();
+            break;
+        case "2":
+            menu.ShowLanguageMenu();
+            break;
+        case "3":
+            Console.Clear();
+            System.Console.WriteLine("Exiting the game...");
+            inMenu = false;
+            break;
+        default:
+            menu.DisplayError();
+            menu.Pause();
+            break;
+    }
+}
